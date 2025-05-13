c64dfc0c0fc834d0260cc9c652e54215b9ca6f81
Lakshya Jain <lakshyajainrj19@gmail.com>
2025-05-13 15:14:03 +0530

    Feat: Setup react router dom and basic routes

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import NotFound from "./pages/NotFound";
+import ProjectDetail from "./pages/ProjectDetail";
+import ApplyPage from "./pages/ApplyPage";
+import Contact from "./pages/Contact";
+import { TerminalProvider } from "./context/TerminalContext";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <TerminalProvider>
+        <BrowserRouter>
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/projects/:projectId" element={<ProjectDetail />} />
+                <Route path="/apply" element={<ApplyPage />} />
+                <Route path="/contact" element={<Contact />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </TerminalProvider>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
